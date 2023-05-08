b4a1be33087ad51844821fd4a15984e51eb461e2
Kirill <zakalupali@gmail.com>
2023-05-08 15:13:08 +0300

    feat: complete task 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.stat(path.join(__dirname, 'secret-folder/data.csv'), (err, stats)=> {
+  if(err) {
+    throw err;
+  }
+  console.log(stats.size);
+});
+
+fs.readdir(path.join(__dirname, 'secret-folder'), {withFileTypes: true},(err, files)=> {
+  if(err) {
+    throw err;
+  }
+  files.forEach(item=> {
+    if(item.isFile()) {
+      fs.stat(path.join(__dirname,'secret-folder', item.name), (err, stats)=> {
+        if(err) {
+          throw err;
+        }
+        console.log(
+          path.parse(item.name).name,
+          ' - ',
+          path.parse(item.name).ext.slice(1),
+          ' - ',
+          (stats.size / 1024) + 'kb');
+      });
+    }
+    // console.log(item.name);
+  });
+});
+
