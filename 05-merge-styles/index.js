3d5c2453a7b04ebc4d11272538575ffd08de49f2
Kirill <zakalupali@gmail.com>
2023-05-08 22:06:00 +0300

    fix: fix task 05-merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -14,9 +14,13 @@ let i = 0;
 function writeToFile(from, to, flags) {
   const readStr = fs.createReadStream(from);
   const writeStr = fs.createWriteStream(to, {flags});
+  if(flags) {
+    writeStr.write('\n');
+  }
   readStr.pipe(writeStr);
   writeStr.on('finish', ()=> {
     i+=1;
+    // writeStr.write('\n');
     const nextFileName = arrStyles[i]?.name;
     if(nextFileName){
       writeToFile(path.join(pathStyles,nextFileName), to, 'a');
