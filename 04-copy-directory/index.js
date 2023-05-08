e6144d617d098b6dc272c1ba53d44cef54ad7814
Kirill <zakalupali@gmail.com>
2023-05-08 16:22:41 +0300

    feat: complete task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,41 @@
+const path = require('path');
+const fs = require('fs');
+
+const fromPath = path.join(__dirname, 'files');
+const toPath = path.join(__dirname, 'files-copy');
+
+function ifError(err) {
+  if(err){
+    throw err;
+  }
+}
+
+function copyDir (from, to) {
+  fs.stat(to, (err)=> {
+    if(err) {
+      fs.mkdir(to, (err)=> {
+        ifError(err);
+      });
+    }
+  });
+
+  fs.readdir(from, {withFileTypes: true}, (err, files)=>{
+    ifError(err);
+
+    files.forEach((item)=> {
+      const newFromPath = path.join(from, item.name);
+      const newToPath = path.join(to,item.name);
+      if(item.isDirectory()) {
+        copyDir(newFromPath, newToPath);
+      }
+      if(item.isFile()){
+        fs.copyFile(newFromPath, newToPath, (err)=> {
+          ifError(err);
+        });
+      }
+    });
+  });
+}
+
+copyDir(fromPath, toPath);
 
