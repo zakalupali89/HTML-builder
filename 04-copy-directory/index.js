5ef6167e47ebc1e359dcf6d4d3493ced85e47de7
Kirill <zakalupali@gmail.com>
2023-05-08 16:53:25 +0300

    fix: fix task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -10,32 +10,41 @@ function ifError(err) {
   }
 }
 
-function copyDir (from, to) {
-  fs.stat(to, (err)=> {
-    if(err) {
-      fs.mkdir(to, (err)=> {
-        ifError(err);
-      });
-    }
-  });
-
-  fs.readdir(from, {withFileTypes: true}, (err, files)=>{
+function copyPath (from, to) {
+  fs.mkdir(to, (err)=> {
     ifError(err);
+    fs.readdir(from, {withFileTypes: true}, (err, files)=>{
+      ifError(err);
 
-    files.forEach((item)=> {
-      const newFromPath = path.join(from, item.name);
-      const newToPath = path.join(to,item.name);
-      if(item.isDirectory()) {
-        copyDir(newFromPath, newToPath);
-      }
-      if(item.isFile()){
-        fs.copyFile(newFromPath, newToPath, (err)=> {
-          ifError(err);
-        });
-      }
+      files.forEach((item)=> {
+        const newFromPath = path.join(from, item.name);
+        const newToPath = path.join(to,item.name);
+        if(item.isDirectory()) {
+          copyDir(newFromPath, newToPath);
+        }
+        if(item.isFile()){
+          fs.copyFile(newFromPath, newToPath, (err)=> {
+            ifError(err);
+          });
+        }
+      });
     });
   });
 }
 
+function copyDir (from, to) {
+  fs.stat(to, (err)=> {
+    if(err) {
+      copyPath(from, to);
+    } else {
+      fs.rm(to,{recursive: true}, (err)=> {
+        ifError(err);
+        copyPath(from, to);
+      });
+    }
+  });
+}
+
 copyDir(fromPath, toPath);
+
 
