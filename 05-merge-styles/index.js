28d8c35b3e79f1d2d317186a89a02b7c49daf8b9
Kirill <zakalupali@gmail.com>
2023-05-08 18:21:50 +0300

    feat: complete task 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const fs = require('fs');
+
+const pathBundle = path.join(__dirname,'project-dist', 'bundle.css');
+const pathStyles = path.join(__dirname,'styles');
+
+function ifError(err) {
+  if(err){
+    throw err;
+  }
+}
+
+let i = 0;
+function writeToFile(from, to, flags) {
+  const readStr = fs.createReadStream(from);
+  const writeStr = fs.createWriteStream(to, {flags});
+  readStr.pipe(writeStr);
+  writeStr.on('finish', ()=> {
+    i+=1;
+    const nextFileName = arrStyles[i]?.name;
+    if(nextFileName){
+      writeToFile(path.join(pathStyles,nextFileName), to, 'a');
+    }
+  });
+}
+
+let arrStyles = [];
+fs.readdir(pathStyles, {withFileTypes: true}, ( err, files)=> {
+  ifError(err);
+  arrStyles = files.filter((item)=> item.isFile() && path.extname(item.name) === '.css');
+  writeToFile(path.join(pathStyles, arrStyles[0]?.name), pathBundle);
+});
+
